d027ffe0174501c08e28cd334ebe7e4829bd4393
Jascha030 <contact@jaschavanaalst.nl>
2022-11-08 13:33:50 +0100

    ♻️ Pass arguments instead of setting global exports.

--- lolmsg.plugin.zsh
+++ lolmsg.plugin.zsh
@@ -1,26 +1,8 @@
-if (( ${+VIM} && ${+VIMRUNTIME} && ${+MYVIMRC} )); then 
-  export _IS_VIMTERM=true 
-else 
-  export _IS_VIMTERM=false
-fi 
-
-# Reset blink effect in case it wasn't ended in previous session.
-if ! (( ${+DOT_DEFAULT_LOL_MSG} )); then 
-    # Normal msg displayed with figlet in funky colors.
-    export DOT_DEFAULT_LOL_MSG="Hackerman Mode 030"   
-fi 
-
-if ! (( ${+DOT_NEOVIM_LOL_MSG} )); then 
-    # Alt msg displayed when in Neovim term.
-    export DOT_NEOVIM_LOL_MSG="NVIM 030"
-fi
-
-if ! (( ${+DOT_PREFER_TPUT_OVER_ENV} )); then 
+if ! (( ${+DOT_PREFER_TPUT_OVER_ENV} )); then
     export DOT_PREFER_TPUT_OVER_ENV=false
 fi
 
 local _LOLMSG_PATH=${0:a:h}
-
 export fpath=( "$_LOLMSG_PATH" ${fpath[@]} )
 
 autoload -Uz "$_LOLMSG_PATH/lolmsg"
@@ -29,4 +11,3 @@ function lolmsg {
     # undefined
     builtin autoload -X
 }
-
